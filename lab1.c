4399204cb8e4adf62fc12b1c84ceef2cfa3a751a
AngryKiwi <59664684+AngryKiwi@users.noreply.github.com>
2020-01-13 15:23:35 -0700

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -18,7 +18,7 @@ int main(){
   
   // passing by references
   readScores(scores, &count);
-  displayScores(scores, &count);
+  displayScores(scores, count);
 
   struct freq hist[count];
   //initiazing the struct hist array
@@ -28,9 +28,9 @@ int main(){
   }
   
   calcHistogram(hist, &count);
-  displayHistogram(hist, &count);
+  displayHistogram(hist, count);
   sortHistogram(hist, &count);
-  displayHistogram(hist, &count);
+  displayHistogram(hist, count);
 }
 
 // readScores takes the file that has the scores and puts the elements in the array
@@ -43,18 +43,18 @@ void readScores(int *pScores, int *pCount){
 }
 
 // displayScores takes the information from readScores and displays the scores
-void displayScores(int *pScores, int *pCount){
+void displayScores(int *pScores, int pCount){
   printf("Displaying the file results\n");
-  for(int i = 0; i < *pCount; i++){ // iterate through the array
+  for(int i = 0; i < pCount; i++){ // iterate through the array
     printf("score %d: %d\n", i, pScores[i]); // displays the array
   }
   printf("\n");
 }
 
 // displayHistogram takes the information from calcHistogram and displays the histogram
-void displayHistogram(struct freq *hist, int *pCount){
+void displayHistogram(struct freq *hist, int pCount){
   printf("Displaying the Histogram\n");
-  for(int i = 0; i < *pCount; i++){
+  for(int i = 0; i < pCount; i++){
     printf("value %d: freq %d\n", hist[i].scoreNum, hist[i].scoreFrequency);
       }
   printf("\n");
@@ -69,7 +69,6 @@ void calcHistogram(struct freq *hist, int *pCount){
 	hist[i].scoreFrequency++; // counts the frequency of how many times each element was counted
       }
     }
-    // printf("%d %d \n", hist[i].scoreNum, hist[i].scoreFrequency);
   }
   
   // update array to not have duplicates
@@ -86,25 +85,22 @@ void calcHistogram(struct freq *hist, int *pCount){
   }
 }
 
-
+// sorts the histogram by the frequency they arrive
 void sortHistogram(struct freq *hist, int *pCount){
-  int minIndex;
-
-  for(int i = *pCount -1; i > 0; i--){
+  int minIndex; // variable to hold the minimum index
+  // selection sort going in descending order
+  for(int i = *pCount - 1; i > 0; i--){
     minIndex = 0;
     for (int j = 1; j <= i; j++){
-      if((hist[j].scoreFrequency < hist[minIndex].scoreFrequency) ||
-	((hist[j].scoreFrequency == hist[minIndex].scoreFrequency)&&
-	 (hist[j].scoreNum < hist[minIndex].scoreNum))) {
-	minIndex = j;
+      if((hist[j].scoreFrequency < hist[minIndex].scoreFrequency) || // looks at the array and compares to the minimum 
+	 ((hist[j].scoreFrequency == hist[minIndex].scoreFrequency)&& // if the score frequencies are equal 
+	 (hist[j].scoreNum < hist[minIndex].scoreNum))) { // and the score numbers is less than the minIndex
+	minIndex = j; // set min index to j
       }
     }
+    // then swap the number in the array
     struct freq tmp = hist[i];
     hist[i] = hist[minIndex];
     hist[minIndex] = tmp;
   }
-  /*
-  for(int i = 0; i < *pCount; i++){
-    printf("%d %d\n", hist[i].scoreNum, hist[i].scoreFrequency);
-    } */
 }
